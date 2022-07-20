0e76e67e899f2de7c07bb1697f9cebc7914af8a9
yzqdev <yzqdev@outlook.com>
2022-07-20 16:22:41 +0800

    update

--- simple/bin/md5.dart
+++ simple/bin/md5.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
+
 import 'package:crypto/crypto.dart' as crypto;
-import 'dart:typed_data';
 
 bool isPhone(String input) {
   RegExp pathReg = RegExp(r"\.\\");
@@ -8,11 +8,11 @@ bool isPhone(String input) {
 }
 
 void main(List<String> arguments) async {
+  DateTime now = DateTime.now();
 
-  var filePath = arguments[0] ;
-  if(isPhone(filePath)){
-    filePath=arguments[0].substring(2);
-
+  var filePath = arguments[0];
+  if (isPhone(filePath)) {
+    filePath = arguments[0].substring(2);
   }
   print(filePath);
   File file = File(filePath);
@@ -21,7 +21,9 @@ void main(List<String> arguments) async {
     final fileStream = file.openRead();
     var md5 = crypto.md5;
     final checksum = (await md5.bind(fileStream).first).toString();
-    print(checksum);
+
+    print(
+        'md51:$checksum，time: ${(DateTime.now().millisecondsSinceEpoch - now.millisecondsSinceEpoch) / 1000}s');
   } catch (e) {
     print(e);
     print("找不到文件");
